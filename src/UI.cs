6385b133c14418c263cc33ec0f4a54bba7188afc
Francis Dominic Fajardo <fajardo@duck.com>
2022-12-24 21:29:41 +0800

    Stop hardcoding UI text and move them to a language file

--- src/UI.cs
+++ src/UI.cs
@@ -10,40 +10,30 @@ namespace StockpileLimit
     [HarmonyPatch(typeof(ThingFilterUI), "DoThingFilterConfigWindow")]
     public class ThingFilterUIWindowPatch
     {
-        public static string buffer;
-        private static ISlotGroupParent selected;
-        private static Dictionary<int, string> menuLimit = new Dictionary<int, string>()
+        private static string _buffer;
+        private static ISlotGroupParent _selected;
+
+        private static readonly string _labelCustom = "stl.custom".Translate();
+        private static readonly Dictionary<int, string> _menuLimit = new Dictionary<int, string>()
         {
-            [-1] = "No limit",
+            [-1] = "stl.limit_set.-1.label".Translate(),
             [0] = "0",
             [1] = "1",
             [2] = "2",
             [5] = "5",
-            [10] = "10",
+            [10] = "10"
         };
-        private static Dictionary<int, string> tooltipLimit = new Dictionary<int, string>()
+        private static readonly Dictionary<int, string> _menuRefill = new Dictionary<int, string>()
         {
-            [-1] = "No limit is default, like vanilla.",
-            [0] = "Keep empty, like there's no stockpile",
+            [100] = "stl.refill_at.100.label".Translate(),
+            [50] = "stl.refill_at.50.label".Translate(),
+            [0] = "stl.refill_at.0.label".Translate()
         };
-        private static string otherlimit_text = "Other limit:";
-        private static Dictionary<int, string> menuRefill = new Dictionary<int, string>()
-        {
-            [100] = "Full",
-            [50] = "Half",
-            [0] = "Empty",
-        };
-        private static Dictionary<int, string> tooltipRefill = new Dictionary<int, string>()
-        {
-            [100] = "Full is default. Always keep fully stocked, like vanilla.",
-            [0] = "Start refilling when empty",
-        };
-        private static string otherrefill_text = "Other percent:";
 
         public static void Prefix(ref Rect rect)
         {
-            selected = Find.Selector.SingleSelectedObject as ISlotGroupParent;
-            if (selected != null)
+            _selected = Find.Selector.SingleSelectedObject as ISlotGroupParent;
+            if (_selected != null)
             {
                 rect.yMin += 90f;
             }
@@ -51,7 +41,7 @@ namespace StockpileLimit
 
         public static void Postfix(ref Rect rect)
         {
-            if (selected == null)
+            if (_selected == null)
             {
                 return;
             }
@@ -63,7 +53,7 @@ namespace StockpileLimit
                 Event.current.Use();
             }
 
-            var settings = selected.GetStoreSettings();
+            var settings = _selected.GetStoreSettings();
 
             DoSetLimitRect(rect, settings);
             DoRefillAtRect(rect, settings);
@@ -78,23 +68,36 @@ namespace StockpileLimit
             drawArea.SplitVerticallyWithMargin(out Rect labelArea, out drawArea, out var _, 5f, 70f);
             drawArea.SplitVerticallyWithMargin(out Rect buttonArea, out drawArea, out var _, 5f, 100f);
             Rect inputArea = drawArea.LeftPartPixels(50f);
-            Widgets.Label(labelArea, "Set limit");
-            TooltipHandler.TipRegion(labelArea, "Set an upper limit for each tile in this stockpile. The limit is strictly in effect for pawns. Pawns will not haul items exceeding the upper limit.");
-            if (tooltipLimit.ContainsKey(limit))
+
+            Widgets.Label(
+                labelArea,
+                "stl.limit_set.label".Translate());
+            TooltipHandler.TipRegion(
+                labelArea,
+                "stl.limit_set.tooltip".Translate());
+
+            string buttonTooltipKey = $"stl.limit_set.{limit}.tooltip";
+            if (buttonTooltipKey.CanTranslate())
             {
-                TooltipHandler.TipRegion(buttonArea, tooltipLimit[limit]);
+                TooltipHandler.TipRegion(buttonArea, buttonTooltipKey.Translate());
             }
-            if (Widgets.ButtonText(buttonArea, menuLimit.ContainsKey(limit) ? menuLimit[limit] : otherlimit_text))
+
+            string buttonLabel = _labelCustom;
+            if (_menuLimit.ContainsKey(limit))
+            {
+                buttonLabel = _menuLimit[limit];
+            }
+            if (Widgets.ButtonText(buttonArea, buttonLabel))
             {
                 var options = new List<FloatMenuOption>(
-                    menuLimit.Select(p => new FloatMenuOption(
+                    _menuLimit.Select(p => new FloatMenuOption(
                         p.Value, () => settings.SetStackLimitAndNotifyChange(p.Key))));
                 Find.WindowStack.Add(new FloatMenu(options));
             }
 
-            buffer = null;
+            _buffer = null;
             var new_limit = limit;
-            Widgets.TextFieldNumeric(inputArea, ref new_limit, ref buffer, -1, AdditionalStorageSettings.MaxLimit);
+            Widgets.TextFieldNumeric(inputArea, ref new_limit, ref _buffer, -1, AdditionalStorageSettings.MaxLimit);
             if (new_limit != limit)
             {
                 settings.SetStackLimitAndNotifyChange(new_limit);
@@ -103,31 +106,46 @@ namespace StockpileLimit
 
         public static void DoRefillAtRect(Rect rect, StorageSettings settings)
         {
-            var refillpercent = settings.GetRefillPercent();
+            var refillPercent = settings.GetRefillPercent();
             Rect drawArea = new Rect(rect.xMin, rect.yMin - 48f - 3f - 60f, rect.width, 24f);
             drawArea.SplitVerticallyWithMargin(out Rect labelArea, out drawArea, out var _, 5f, 70f);
             drawArea.SplitVerticallyWithMargin(out Rect buttonArea, out drawArea, out var _, 5f, 100f);
             drawArea.SplitVerticallyWithMargin(out Rect inputArea, out drawArea, out var _, 5f, 50f);
-            Widgets.Label(labelArea, "Refill at");
 
-            TooltipHandler.TipRegion(labelArea, "Set the refill threshold of *each tile* in this stockpile. This can be considered as the lower limit.");
+            Widgets.Label(
+                labelArea,
+                "stl.refill_at.label".Translate());
+            TooltipHandler.TipRegion(
+                labelArea,
+                "stl.refill_at.tooltip".Translate());
 
-            if (tooltipRefill.ContainsKey(refillpercent))
+            string buttonTooltipKey = $"stl.refill_at.{refillPercent}.tooltip";
+            if (buttonTooltipKey.CanTranslate())
             {
-                TooltipHandler.TipRegion(buttonArea, tooltipRefill[refillpercent]);
+                TooltipHandler.TipRegion(buttonArea, buttonTooltipKey.Translate());
             }
 
-            if (Widgets.ButtonText(buttonArea, menuRefill.ContainsKey(refillpercent) ? menuRefill[refillpercent] : otherrefill_text))
+            string buttonLabel =  _labelCustom;
+            if (_menuRefill.ContainsKey(refillPercent))
             {
-                var options = new List<FloatMenuOption>(menuRefill.Select(p => new FloatMenuOption(p.Value, () =>
-                settings.SetRefillPercent(p.Key))));
+                buttonLabel = _menuRefill[refillPercent];
+            }
+            if (Widgets.ButtonText(buttonArea, buttonLabel))
+            {
+                List<FloatMenuOption> options = new List<FloatMenuOption>(
+                    _menuRefill.Select(p => new FloatMenuOption(
+                        p.Value, () => settings.SetRefillPercent(p.Key))));
                 Find.WindowStack.Add(new FloatMenu(options));
             }
 
-            buffer = null;
-            var new_refillpercent = refillpercent;
-            Widgets.TextFieldNumeric(inputArea, ref new_refillpercent, ref buffer, 0, 100);
-            if (new_refillpercent != refillpercent) settings.SetRefillPercent(new_refillpercent);
+            _buffer = null;
+            var new_refillpercent = refillPercent;
+            Widgets.TextFieldNumeric(inputArea, ref new_refillpercent, ref _buffer, 0, 100);
+            if (new_refillpercent != refillPercent)
+            {
+                settings.SetRefillPercent(new_refillpercent);
+            }
+
             Widgets.Label(drawArea, "%");
         }
 
@@ -136,8 +154,16 @@ namespace StockpileLimit
             Rect drawArea = new Rect(rect.xMin, rect.yMin - 48f - 3f - 30f, rect.width, 24f);
             var checkOn = settings.IsRefillingDisabled();
             var new_checkOn = checkOn;
-            TooltipHandler.TipRegion(drawArea, "Disable refilling for all tiles in this stockpile.");
-            Widgets.CheckboxLabeled(drawArea, "Pause refilling", ref new_checkOn, placeCheckboxNearText: true);
+
+            TooltipHandler.TipRegion(
+                drawArea,
+                "stl.refill_pause.tooltip".Translate());
+            Widgets.CheckboxLabeled(
+                drawArea,
+                "stl.refill_pause.label".Translate(),
+                ref new_checkOn,
+                placeCheckboxNearText: true);
+
             if (new_checkOn != checkOn)
             {
                 if (new_checkOn)
